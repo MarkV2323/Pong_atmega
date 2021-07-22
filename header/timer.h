77f14295782162681be422ed4736ebb42b81196f
Mark Vincent <41304553+MarkV2323@users.noreply.github.com>
2021-07-22 16:24:29 -0700

    Added Draw method, setup project further.

---
+++ header/timer.h
@@ -0,0 +1,92 @@
+// Must include
+#include <avr/interrupt.h>
+#define tasksSize 1
+#define tasksGCD 1
+
+// Important Stuff
+// Task Structure
+typedef struct Task {
+    int state;
+    unsigned long period;
+    unsigned long elapsedTime;
+    int (*TickFct) (int);
+} Task;
+
+// Globals for Task
+// unsigned char tasksSize = 2;
+Task tasks[tasksSize];
+
+
+
+// TimerISR() sets this to 1. C programmer should clear to 0.
+volatile unsigned char TimerFlag = 0;
+
+// Internal variables for mapping AVR's ISR to our cleaner TimerISR model.
+unsigned long _avr_timer_M = 1; // Start count from here, to 0. Default 1ms
+unsigned long _avr_timer_cntcurr = 0; // Current internal count of 1 ms ticks.
+
+// TimerOn Function
+void TimerOn() {
+    // AVR timer/counter controller register TCCR1
+    TCCR1B = 0x0B;
+    // bit3 = 0: CTC mode (clear timer on compare)
+    // bit2bit1bit0=011: pre-scaler /64
+    // 00001011 : 0x0B
+    // SO, 8 MHz clock or 8mill / 64 = 125,000 ticks / second
+    // => TCNT1 register will count at 125,000 ticks / second
+
+    // AVR output compare register OCR1A.
+    OCR1A = 125;
+    // Timer interrupt will be gen when TCNT1 == OCR1A
+    // We want 1 ms tick, 0.001 s * 125,000 t/s = 125
+    // When TCNT1 register equals 125, 1 ms has passed.
+
+    // AVR timer interrupt mask register
+    TIMSK1 = 0x02; // bit1: OCIE1A -- enable compare match interrupt
+
+    // Init avr counter
+    TCNT1 = 0;
+
+    _avr_timer_cntcurr = _avr_timer_M;
+    // TimerISR will be called every _avr_timer_cntcurr ms
+
+    // enable global interrupts
+    SREG |= 0x80; // 0x80: 1000 0000
+
+}
+
+// TimerOff Function
+void TimerOff() {
+    TCCR1B = 0x00; //bit3bit1bit0 0-000: timer off
+}
+
+// TimerISR Function
+void TimerISR() {
+    unsigned char i;
+    for (i = 0; i < tasksSize; ++i) {
+        if (tasks[i].elapsedTime >= tasks[i].period) { // If task is ready
+            // execute the tick function and update state varible.
+            tasks[i].state = tasks[i].TickFct(tasks[i].state);
+            tasks[i].elapsedTime = 0; // Reset elapsed time
+        }
+        tasks[i].elapsedTime += tasksGCD; // Update elapsed time
+    }
+}
+
+// C programmer does not touch this ISR, but rather TimerISR()
+ISR(TIMER1_COMPA_vect) {
+    // CPU auto calls when TCNT1 == OCR1 (every 1 ms per TimerOn settings)
+    _avr_timer_cntcurr--; // count down to 0 rather than up to TOP
+    if (_avr_timer_cntcurr == 0) { // efficient compare
+        TimerISR(); // Call the ISR that the user will use
+        _avr_timer_cntcurr = _avr_timer_M;
+    }
+
+}
+
+// Set TimerISR() to tick every M ms
+void TimerSet(unsigned long M) {
+    _avr_timer_M = M;
+    _avr_timer_cntcurr = _avr_timer_M;
+}
+// END OF TIMER FUNCTIONS
--- source/main.c
+++ source/main.c
@@ -1,23 +1,161 @@
-/*	Author: lab
- *  Partner(s) Name: 
- *	Lab Section:
- *	Assignment: Lab #  Exercise #
- *	Exercise Description: [optional - include for your own benefit]
- *
+/*	Author: Mark Alan Vincent II
+ *  Partner(s) Name: NA
+ *	Lab Section: A01
+ *	Assignment: Final Project
+ *	Youtube:
+ *  Exercise Description:
  *	I acknowledge all content contained herein, excluding template or example
  *	code, is my own original work.
  */
 #include <avr/io.h>
 #ifdef _SIMULATE_
 #include "simAVRHeader.h"
+#include "timer.h"
 #endif
 
-int main(void) {
-    /* Insert DDR and PORT initializations */
-
-    /* Insert your solution below */
-    while (1) {
-
+// Method for handling writing to register.
+// Modified: RCLK (C2 and C4) and SRCLR(C3 and C5) are independent of each other.
+void transmit_data(unsigned char data, unsigned char regi) {
+    int i;
+    for (i = 0; i < 8 ; ++i) {
+   	 // Sets SRCLR to 1 allowing data to be set
+   	 // Also clears SRCLK in preparation of sending data
+     if (regi == 1) {
+       PORTC = 0x08;
+     } else if (regi == 2) {
+       PORTC = 0x20;
+     }
+   	 // set SER = next bit of data to be sent.
+   	 PORTC |= ((data >> i) & 0x01);
+   	 // set SRCLK = 1. Rising edge shifts next bit of data into the shift register
+   	 PORTC |= 0x02;
     }
+
+    // set RCLK = 1. Rising edge copies data from “Shift” register to “Storage” register
+    if (regi == 1) {
+      PORTC |= 0x04;
+    } else if (regi == 2) {
+      PORTC |= 0x10;
+    }
+
+    // clears all lines in preparation of a new transmission
+    PORTC = 0x00;
+}
+
+// Function for A2D conversion
+void A2D_init() {
+      ADCSRA |= (1 << ADEN) | (1 << ADSC) | (1 << ADATE);
+	// ADEN: Enables analog-to-digital conversion
+	// ADSC: Starts analog-to-digital conversion
+	// ADATE: Enables auto-triggering, allowing for constant
+	//	    analog to digital conversions.
+}
+
+// globals
+unsigned char tmpA = 0x00;
+
+// Contains frames of the current board.
+unsigned char frames_x[] = {0x70, 0x00, 0x00, 0x00, 0x0E};
+unsigned char frames_y[] = {0x1E, 0x1D, 0x0B, 0x17, 0x0F}; // dnt modifiy
+unsigned char frames_index = 0;
+unsigned long count = 0;
+
+enum SM1_States { SM1_Start, SM1_Draw } sm1_state;
+int TickFct_DrawGame(int state) {
+
+      // Transitions
+      switch (state) {
+          case SM1_Start:
+              // Go to Draw state.
+              state = SM1_Draw;
+              break;
+
+          case SM1_Draw:
+              // Continue Drawing.
+              state = SM1_Draw;
+              break;
+
+          default:
+              break;
+      }
+
+      //Actions
+      switch (state) {
+           case SM1_Start:
+            break;
+
+           case SM1_Draw:
+            // Draws 5 Frames, top down.
+            if (frames_index == 4) {
+              // draw last frame and reset
+              transmit_data(frames_y[frames_index], 2);
+              transmit_data(frames_x[frames_index], 1);
+              transmit_data(0x00, 1);
+
+              // transmit_data(0x0F, 1);
+              // transmit_data(0x1E, 2);
+              // transmit_data(0x00, 1);
+              // transmit_data(0x0F, 2);
+              // transmit_data(0x0E, 1);
+              // transmit_data(0x00, 1);
+              // transmit_data(0x1E, 2);
+              frames_index = 0;
+            } else {
+              // draw and increment frame
+              transmit_data(frames_y[frames_index], 2);
+              transmit_data(frames_x[frames_index], 1);
+              transmit_data(0x00, 1);
+              frames_index = frames_index + 1;
+
+              // transmit_data(0x0F, 1);
+              // transmit_data(0x1E, 2);
+              // transmit_data(0x00, 1);
+              // transmit_data(0x0F, 2);
+              // transmit_data(0x0E, 1);
+              // transmit_data(0x00, 1);
+              // transmit_data(0x1E, 2);
+            }
+            count++;
+            if (count == 1000) {
+              // shift player1 Left
+              frames_x[0] = frames_x[0] >> 1;
+              count = 0;
+            }
+            break;
+
+           default:
+            break;
+      }
+
+      return state;
+}
+
+
+// Main Funcntion
+int main(void) {
+
+    // PORTS
+    DDRA = 0x00; PORTA = 0xFF; // PortA as input
+    DDRB = 0xFF; PORTB = 0x00; // PortB as output
+    DDRC = 0xFF; PORTC = 0x00; // PortC as output
+    DDRD = 0xFF; PORTD = 0x00; // PortD as output
+
+    // Start A2D conversion
+    // A2D_init();
+
+    // Setup Task List
+    unsigned char i = 0;
+
+    // SM1 (Draw SM)
+    tasks[i].state = SM1_Start;
+	  tasks[i].period = 1;
+	  tasks[i].elapsedTime = tasks[i].period;
+	  tasks[i].TickFct = &TickFct_DrawGame;
+	  //i++;
+
+    // Setup System Period & Timer to ON.
+    TimerSet(tasksGCD);
+    TimerOn();
+    while (1) {}
     return 1;
 }
